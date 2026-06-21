cfe9f6817964eeae05821a4ccd62f6262278c0e2
Frank chetsarit <am2.production29@gmail.com>
2026-06-22 00:56:47 +0700

    Import glass top bar override

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 import './ui-editor.css'
 import './flow-redesign.css'
+import './glass-topbar.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
