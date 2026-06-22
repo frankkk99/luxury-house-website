451793443534ded8c3edade40591a89ed9577870
Frank chetsarit <am2.production29@gmail.com>
2026-06-22 21:24:18 +0700

    Simplify app entry imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import './ui-editor.css'
-import './flow-redesign.css'
-import './glass-topbar.css'
-import './mobile-ui-editor.css'
-import './mobile-luxury-home.css'
-import './mobile-ui-editor.js'
-import './force-mobile-compact.js'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
